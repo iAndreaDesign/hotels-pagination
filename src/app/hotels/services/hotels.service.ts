b2dd3e66ab5d6d94ba46ea28c8e9c30c0c0be794
Andrea <iandrea.design@gmail.com>
2024-12-03 11:07:43 +0100

    Volver al proyecto abse

--- src/app/hotels/services/hotels.service.ts
+++ src/app/hotels/services/hotels.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable, signal } from '@angular/core';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { Observable, Subject, exhaustMap, map } from 'rxjs';
+import { Inject, Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
 import { Hotel } from '../models/hotel.model';
 
 @Injectable({
@@ -9,33 +8,23 @@ import { Hotel } from '../models/hotel.model';
 })
 export class HotelsService {
   private readonly bdUrl: string = 'http://localhost:3000/hotels';
-  private readonly httpClient: HttpClient = inject(HttpClient);
-  getHotels$ = new Subject<Hotel[]>();
 
-  constructor() {
-    this.getHotels$.pipe(
-      takeUntilDestroyed(),
-      exhaustMap(() => this.httpClient.get<Hotel[]>(`${this.bdUrl}`)
-      )
-    );
+  constructor(private httpCLient: HttpClient) {}
+
+  getHotels(): Observable<Hotel[]> {
+    return this.httpCLient.get<Hotel[]>(`${this.bdUrl}`);
   }
 
-
   getHotelsfilterByName(name: string): Observable<Hotel[]> {
-    return this.getHotels$.pipe(
-      takeUntilDestroyed(),
-      exhaustMap(()=> 
-        this.getHotels$.pipe(
-          map((hotels) => hotels.filter((hotel) => {
-              return hotel.name.toLowerCase().includes(name.toLowerCase())
-            })
-        ))
-      )
-    )
+    return this.getHotels().pipe(
+      map((hotels) => hotels.filter((hotel) => {
+          return hotel.name.toLowerCase().includes(name.toLowerCase())
+        })
+    ))
   }
 
   getFilteredHotels(name: string = '', categories: number[] = [], minRating: number = 0, maxPrice: number = 10000): Observable<Hotel[]> {
-    return this.getHotels$.pipe(
+    return this.getHotels().pipe(
       map((hotels) => hotels.filter((hotel) => {
         return (
           (name === '' || hotel.name.toLowerCase().includes(name.toLowerCase())) &&
@@ -46,5 +35,4 @@ export class HotelsService {
       }))
     );
   }
-
 }
